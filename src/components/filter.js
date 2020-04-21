9c040b08f4f77b46ab000d4fc201b5b59c14033d
Beloy Andrey <bagmobile@yandex.ru>
2020-04-21 21:42:44 +0300

    Учебный проект: разделяй и властвуй (Часть 1)

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,9 +1,10 @@
-import {createElement} from "../utils.js";
+import AbstractComponent from "./abstract-component.js";
 
 const createFilterMarkup = (filter, isChecked) => {
   const {name, count} = filter;
 
-  return (`<input
+  return (
+    `<input
       type="radio"
       id="filter__${name}"
       class="filter__input visually-hidden"
@@ -12,36 +13,26 @@ const createFilterMarkup = (filter, isChecked) => {
     />
     <label for="filter__${name}" class="filter__label">
       ${name} <span class="filter__${name}-count">${count}</span></label
-    >`);
+    >`
+  );
 };
 
 const createFilterTemplate = (filters) => {
-  const filtersMarkup = filters.map((filter, index) => createFilterMarkup(filter, index === 0)).join(`\n`);
+  const filtersMarkup = filters.map((it, i) => createFilterMarkup(it, i === 0)).join(`\n`);
 
-  return (`<section class="main__filter filter container">
+  return `<section class="main__filter filter container">
     ${filtersMarkup}
-   `);
+  </section>`;
 };
 
-export default class Filter {
+export default class Filter extends AbstractComponent {
   constructor(filters) {
+    super();
+
     this._filters = filters;
-    this._element = null;
   }
 
   getTemplate() {
     return createFilterTemplate(this._filters);
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
--- src/components/task.js
+++ src/components/task.js
@@ -1,22 +1,14 @@
+import AbstractComponent from "./abstract-component.js";
 import {MONTH_NAMES} from "../const.js";
-import {createElement, formatTime} from "../utils.js";
+import {formatTime} from "../utils/common.js";
 
 
-// Функцию для генерации HTML-разметки можно превратить в метод класса,
-// однако делать мы этого не будем, потому что это не критично,
-// а функция у нас уже была описана
 const createTaskTemplate = (task) => {
-  // Обратите внимание, что всю работу мы производим заранее.
-  // Внутри шаблонной строки мы не производим никаких вычислений,
-  // потому что внутри большой разметки сложно искать какой-либо код
   const {description, dueDate, color, repeatingDays, isArchive, isFavorite} = task;
-
   const isExpired = dueDate instanceof Date && dueDate < Date.now();
   const isDateShowing = !!dueDate;
-
   const date = isDateShowing ? `${dueDate.getDate()} ${MONTH_NAMES[dueDate.getMonth()]}` : ``;
   const time = isDateShowing ? formatTime(dueDate) : ``;
-
   const repeatClass = Object.values(repeatingDays).some(Boolean) ? `card--repeat` : ``;
   const deadlineClass = isExpired ? `card--deadline` : ``;
   const archiveButtonInactiveClass = isArchive ? `` : `card__btn--disabled`;
@@ -39,14 +31,17 @@ const createTaskTemplate = (task) => {
             favorites
           </button>
         </div>
+
         <div class="card__color-bar">
           <svg class="card__color-bar-wave" width="100%" height="10">
             <use xlink:href="#wave"></use>
           </svg>
         </div>
+
         <div class="card__textarea-wrap">
           <p class="card__text">${description}</p>
         </div>
+
         <div class="card__settings">
           <div class="card__details">
             <div class="card__dates">
@@ -64,26 +59,19 @@ const createTaskTemplate = (task) => {
   </article>`;
 };
 
-export default class Task {
+export default class Task extends AbstractComponent {
   constructor(task) {
-    this._task = task;
+    super();
 
-    this._element = null;
+    this._task = task;
   }
 
   getTemplate() {
     return createTaskTemplate(this._task);
   }
 
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
+  setEditButtonClickHandler(handler) {
+    this.getElement().querySelector(`.card__btn--edit`)
+      .addEventListener(`click`, handler);
   }
 }
